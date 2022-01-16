c61e9dcddf2174f0d71f63d349ff8eeb642d5dcb
George Zhai <glzhai2@illinois.edu>
2022-01-16 15:40:48 -0600

    uber2.0

---
+++ slices/navSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+//state
+const initialState = {
+  origin: null,
+  destination: null,
+  travelTimeInformation: null,
+};
+
+export const navSlice = createSlice({
+  name: "nav",
+  initialState,
+  reducers: {
+    setOrigin: (state, action) => {
+      state.origin = action.payload; // data inside action
+    },
+    setDestination: (state, action) => {
+      state.destination = action.payload;
+    },
+    setTravelTimeInformation: (state, action) => {
+      state.travelTimeInformation = action.payload;
+    },
+  },
+});
+
+export const { setOrigin, setDestination, setTravelTimeInformation } =
+  navSlice.actions;
+
+//Selector
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
+export const selectTravelTimeInformation = (state) =>
+  state.nav.travelTimeInformation;
+
+export default navSlice.reducer;
